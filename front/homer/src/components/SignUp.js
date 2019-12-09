2380e02ee0958b0677fec20e2496e9f0d9f288cd
dolfant <louis.cascio@hotmail.fr>
2019-12-09 15:52:07 +0100

    feat(SignUp):submit display

--- front/homer/src/components/SignUp.js
+++ front/homer/src/components/SignUp.js
@@ -3,23 +3,68 @@ import React, { Component } from 'react';
 class SignUp extends Component {
 
   state = {
-    email: null
+    email: "mon@email.com",
+    password: "password",
+    name: "name",
+    lastname: "lastname"
   }
 
   updateEmailField = (e) => {
     this.setState({ email: e.target.value })
   }
 
+  updatePasswordField = (e) => {
+    this.setState({ password: e.target.value })
+  }
+
+  updateNameField = (e) => {
+    this.setState({ name: e.target.value })
+  }
+
+  updateLastnameField = (e) => {
+    this.setState({ lastname: e.target.value })
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("A user was submited", this.state)
+  }
+
   render() {
     return (
-      <form>
-        <h1>{this.state.email}</h1>
+      <form onSubmit={(e) => this.handleSubmit(e)}>
+        <h1>
+          {JSON.stringify(this.state, 1, 1)}
+        </h1>
         <input
           type="email"
           name="email"
+          placeholder="mon@email.com"
           value={this.state.email}
           onChange={(e) => this.updateEmailField(e)}
         />
+        <input
+          type="password"
+          name="password"
+          placeholder="password"
+          value={this.state.password}
+          onChange={(e) => this.updatePasswordField(e)}
+        />
+        <input
+          type="text"
+          name="name"
+          placeholder="name"
+          value={this.state.name}
+          onChange={(e) => this.updateNameField(e)}
+        />
+        <input
+          type="text"
+          name="lastname"
+          placeholder="lastname"
+          value={this.state.lastname}
+          onChange={(e) => this.updateLastnameField(e)}
+        />
+        <input type="submit" value="Envoyer" />
       </form>
     );
   }
