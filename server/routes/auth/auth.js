805beaf7afbde96de19b9a3e53c68a231c822e59
dolfant <louis.cascio@hotmail.fr>
2019-12-10 12:25:48 +0100

    feat(SignUp): adding flash state message2

--- server/routes/auth/auth.js
+++ server/routes/auth/auth.js
@@ -20,11 +20,10 @@ router.get('/all', (req, res, next) => {
 router.post('/signup', (req, res, next) => {
   const formData = req.body;
   connection.query('INSERT INTO users SET ?', formData, (err, results) => {
-    if (err) {
-      res.status(500).send("Erreur lors de l'ajout d'un user");
-    } else {
-      res.sendStatus(200);
-    }
+    if (err)
+      res.status(500).json({ flash: err.message });
+    else
+      res.status(200).json({ flash: "User has been signed up !" });
   })
 });
 
